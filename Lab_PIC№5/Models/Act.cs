7c5024b6d2de9506fdfa90710e196fdcfd892496
Joleg1c-c <Joleg1c-c@mail.ru>
2023-05-27 13:34:02 +0500

    Организация порядка -Распределение файлов по папкам

---
+++ Lab_PIC№5/Models/Act.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_PIC_5
+{
+    class Act
+    {
+        int actNumber;
+        DateTime date;
+        string organization;
+        string contracts;
+        string application;
+        string animalCard;
+
+        public Act(int actNumberTake, DateTime dateTake, string organizationTake, string contractsTake, string applicationTake, string animalCardTake)
+        {
+            actNumber = actNumberTake;
+            date = dateTake;
+            organization = organizationTake;
+            contracts = contractsTake;
+            application = applicationTake;
+            animalCard = animalCardTake;
+        }
+
+    }
+}
